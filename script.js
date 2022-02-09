10564186d11166f6cb0c5516743d5cb5552fcc24
Eduardo Magalhães Soares <90562184+EduMSoares@users.noreply.github.com>
2022-02-08 21:21:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+// caso todos os dados estejam preenchidos, armazenar dados
+let btn = document.querySelector('button.button')
+
+btn.addEventListener('click', function (){
+
+let email = document.getElementById('email')
+let senha = String( document.getElementById('idSenha') ).valueOf
+let identific = String( document.getElementById('identific') ).valueOf
+
+  if (email.value == '' || senha.value == '' || identific == ''){
+
+    alert('Por favor, verifique todos os campos')
+
+  }else {
+
+    let dados = [
+      email,
+      senha,
+      identific
+    ]
+
+  }
+})
+
+// Caso a checkbox esteja marcada, guardar dados em outro array
+ let box = document.querySelector('input.check')
+ if (box.checked){
+   let lembrarDados = [
+     email,
+     senha,
+     identific
+   ]
+ } else{}
+
+
+// btn-outline hover
+btn.addEventListener('mouseenter', function (){
+  
+  this.style.backgroundColor = "#A5D785"
+  this.style.color = "white"
+
+})
+btn.addEventListener('mouseout', function (){
+  
+  this.style.backgroundColor = "white"
+  this.style.color = "#BBE1A4"
+
+})
